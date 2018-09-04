a1f18bcde91d2868c0d4a0f218617de18f7c11f1
Etienne Baqué <etiennebaque@gmail.com>
2018-09-03 22:08:30 -0400

    Created app summary component

---
+++ app/javascript/packs/components/AppSummary.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Trade or View your portfolio</h2>
+    <p>You may Save & Load your data.</p>
+    <p>Click 'End day' to begin a new day!</p>
+
+    <div>
+      Your Funds: ${{ funds }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    funds: 0;
+  }
+};
+</script>
+
+<style scoped>
+</style>
